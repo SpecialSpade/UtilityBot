5f93ac977850848c6ec572961965559a15f70a85
Martin <quy1.dang@uni-a.de>
2026-03-12 22:59:01 +0100

    Removed unnecessary local variable.

--- src/main/java/specialspade/utilitybot/Application/Message/MessageTypes/UserMessage.java
+++ src/main/java/specialspade/utilitybot/Application/Message/MessageTypes/UserMessage.java
@@ -3,19 +3,18 @@ package specialspade.utilitybot.Application.Message.MessageTypes;
 
 public abstract class UserMessage {
     protected final String messageType;
-    protected final boolean requireImmediateResponse = false;
 
-    public UserMessage(String messageType){
+    public UserMessage(String messageType) {
         this.messageType = messageType;
     }
 
-    public String getMessageType(){
+    public String getMessageType() {
         return messageType;
     }
 
 
-    public boolean requireImmediateResponse(){
-        return requireImmediateResponse;
+    public boolean requireImmediateResponse() {
+        return false;
     }
 
 
